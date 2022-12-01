46d6ba458b9f5adf24233aa22847b820908dcd83
maccony202 <73612968+maccony202@users.noreply.github.com>
2022-12-01 11:09:41 +0000

    Add changes to javascript file

--- main.js
+++ main.js
@@ -2,66 +2,63 @@ const humburger = document.querySelector('.humburger');
 const mobileMenu = document.querySelector('.nav-links');
 
 humburger.addEventListener('click', () => {
-    humburger.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-    document.body.style.overflow = 'hidden';
-
+  humburger.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+  document.body.style.overflow = 'hidden';
 });
 
 document.querySelectorAll('.nav-links').forEach((n) => n
-   .addEventListener('click', () => {
-      humburger.classList.remove('active');
-      mobileMenu.classList.remove('active');
-      document.body.style.overflow = 'auto';
-   })
-);
+  .addEventListener('click', () => {
+    humburger.classList.remove('active');
+    mobileMenu.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }));
 
 const speakerSection = document.querySelector('.speakers-cont');
 
-
 const myArray = [
-    {
-      speakerImg: 'src/assets/images/Otumfuo-Osei-Tutu-II.jpg',
-      name: 'Otumfuo Osei Tutu II',
-      position: 'King of the Ashanti Kingdom',
-      bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana'
-    },
+  {
+    speakerImg: 'src/assets/images/Otumfuo-Osei-Tutu-II.jpg',
+    name: 'Otumfuo Osei Tutu II',
+    position: 'King of the Ashanti Kingdom',
+    bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana',
+  },
 
-    {
-        speakerImg: 'src/assets/images/Togbe.jpg',
-        name: 'Otumfuo Osei Tutu II',
-        position: 'King of the Ashanti Kingdom',
-        bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana'
-      },
+  {
+    speakerImg: 'src/assets/images/Togbe.jpg',
+    name: 'Otumfuo Osei Tutu II',
+    position: 'King of the Ashanti Kingdom',
+    bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana',
+  },
 
-      {
-        speakerImg: 'src/assets/images/Dr-Ibrahim-Mohammed-Awal.jpeg',
-        name: 'Otumfuo Osei Tutu II',
-        position: 'King of the Ashanti Kingdom',
-        bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana'
-      },
+  {
+    speakerImg: 'src/assets/images/Dr-Ibrahim-Mohammed-Awal.jpeg',
+    name: 'Otumfuo Osei Tutu II',
+    position: 'King of the Ashanti Kingdom',
+    bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana',
+  },
 
-      {
-        speakerImg: 'src/assets/images/okraku.jpg',
-        name: 'Otumfuo Osei Tutu II',
-        position: 'King of the Ashanti Kingdom',
-        bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana'
-      },
+  {
+    speakerImg: 'src/assets/images/okraku.jpg',
+    name: 'Otumfuo Osei Tutu II',
+    position: 'King of the Ashanti Kingdom',
+    bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana',
+  },
 
-      {
-        speakerImg: 'src/assets/images/sarkodie.jpg',
-        name: 'Otumfuo Osei Tutu II',
-        position: 'King of the Ashanti Kingdom',
-        bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana'
-      },
+  {
+    speakerImg: 'src/assets/images/sarkodie.jpg',
+    name: 'Otumfuo Osei Tutu II',
+    position: 'King of the Ashanti Kingdom',
+    bio: 'He ensured peace at the time of long standing cheftecy war at bawku the northen part of ghana',
+  },
 ];
 
 const displaySpeakers = ({
-    speakerImg, name, position, bio
+  speakerImg, name, position, bio,
 }) => {
-    const div = document.createElement('div');
-    div.className = 'speakers';
-    div.innerHTML = `
+  const div = document.createElement('div');
+  div.className = 'speakers';
+  div.innerHTML = `
     <img src="${speakerImg}" alt="${name}">
     <div class="speaker-info">
         <h4 class="speaker-name">${name}</h4>
@@ -70,14 +67,13 @@ const displaySpeakers = ({
         <p class="speaker-b">${bio}</p>
     </div>
     `;
-    return (div);
+  return (div);
 };
 
 const getSpeakers = () => {
-    myArray.forEach((myObjects, index) => {
-      speakerSection.append(displaySpeakers(myObjects, index));
-    });
-  };
+  myArray.forEach((myObjects, index) => {
+    speakerSection.append(displaySpeakers(myObjects, index));
+  });
+};
 
-  getSpeakers();
-
+getSpeakers();
